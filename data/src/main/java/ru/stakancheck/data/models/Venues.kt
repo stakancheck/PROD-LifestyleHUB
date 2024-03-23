e70a33d3adf1fade483568439902f69f74997308
stakancheck <stakancheck@gmail.com>
2024-03-24 01:47:09 +0300

    docs(data): 📚 Update Interests data class docs

--- data/src/main/java/ru/stakancheck/data/models/Venues.kt
+++ data/src/main/java/ru/stakancheck/data/models/Venues.kt
@@ -9,11 +9,18 @@
 package ru.stakancheck.data.models
 
 /**
- * Typealias for a list of [Venue] objects.
+ * Typealias for a list of [Interest] objects.
  */
 typealias Interests = List<Interest>
 
 
+/**
+ * Sealed interface representing an Interest.
+ * It can be used in different views and lists to present data of defferent interests.
+ *
+ * _Possible types:_
+ * - [Venue]
+ */
 sealed interface Interest {
     /**
      * Data class representing a Venue.
@@ -23,7 +30,7 @@ sealed interface Interest {
      * @property name The name of the venue.
      * @property address The address of the venue.
      * @property distance The distance to the venue.
-     * @property categories The list of categories of the venue.
+     * @property categories The list of categories of the venue list of [Category].
      * @property photoUrl The URL of the venue's photo in original size.
      * @property photoId The unique identifier for the venue's photo.
      */
@@ -39,7 +46,7 @@ sealed interface Interest {
     ) : Interest {
 
         /**
-         * Data class representing a Category.
+         * Data class representing a Category of [Venue].
          *
          * @property id The unique identifier for the category.
          * @property shortName The short name of the category.
